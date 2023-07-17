4e02b1f4bb8390883c2bc56279831dce5b84dd80
heneralovsem <semm297@gmail.com>
2023-07-17 15:30:27 +0300

    commit

--- src/components/RoomItem/RoomItem.tsx
+++ src/components/RoomItem/RoomItem.tsx
@@ -1,13 +1,26 @@
 import React, {FC, useContext, useState} from 'react'
 import { IRoom } from '../../types/types';
-import { Button } from '@mui/material';
+import { Avatar, Button } from '@mui/material';
 import { Context, RoomContext } from '../..';
+import { collection, query, orderBy } from 'firebase/firestore';
+import { IMessage } from '../../types/types';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 interface RoomItemProps {
-    room: IRoom
+    room: IRoom;
 }
 
+
 const RoomItem: FC<RoomItemProps> = ({room}) => {
+const { auth, firestore } = useContext(Context);
+const [messages, loading] = useCollectionData<IMessage>(
+    query(
+        collection(firestore, `rooms/${room.name}/messages`),
+        orderBy("createdAt")
+    )
+    );
+const lastMessage = messages?.[messages?.length - 1]
+console.log(lastMessage)
 const {selectedRoom, setSelectedRoom} = useContext(RoomContext)
 const selectRoom = () => {
     setSelectedRoom(room.name)
@@ -15,8 +28,12 @@ const selectRoom = () => {
 
     
     return (
-        <div>
-            <Button onClick={selectRoom}>{room.name}</Button>
+        <div onClick={selectRoom}>
+            <h1 onClick={selectRoom}>{room.name}</h1>
+            {lastMessage && <div> <Avatar src={lastMessage?.photoURL}/>
+            <span>{lastMessage?.displayName}</span>
+            <p>{lastMessage?.text}</p> </div> }
+           
         </div>
     )
 }
