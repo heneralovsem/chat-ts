69c1ed60cc516f97dda2f46a8e961be7a3e82ccb
heneralovsem <semm297@gmail.com>
2023-09-22 17:10:05 +0300

    emoji fix

--- src/components/Emojis/Emojis.tsx
+++ src/components/Emojis/Emojis.tsx
@@ -1,8 +1,7 @@
 import React, { FC } from "react";
 import cl from "./Emojis.module.css";
-import EmojiPicker from "emoji-picker-react";
+import EmojiPicker, { Emoji } from "emoji-picker-react";
 import { EmojiClickData } from "emoji-picker-react";
-
 
 interface EmojisProps {
   value: string;
@@ -10,34 +9,20 @@ interface EmojisProps {
   setShowEmojis: (name: boolean) => void;
 }
 
-
 const Emojis: FC<EmojisProps> = ({ value, setValue, setShowEmojis }) => {
   const onEmojiClick = (emojiData: EmojiClickData, event: MouseEvent) => {
-    console.log(emojiData)
     setValue(value + emojiData.emoji);
     setShowEmojis(false);
   };
 
   return (
     <div className={cl.emojis__wrapper}>
-      <EmojiPicker autoFocusSearch={false} previewConfig={{showPreview: false}} width={300} onEmojiClick={onEmojiClick} customEmojis={[{
-        names: ['AYAYA'],
-        imgUrl:
-          'https://cdn3.emoji.gg/emojis/AYAYA.png',
-        id: 'AYAYA'
-      },
-      {
-        names: ['Hat'],
-        imgUrl:
-          'https://cdn.jsdelivr.net/gh/ealush/emoji-picker-react@custom_emojis_assets/hat.png',
-        id: 'hat'
-      },
-      {
-        names: ['Shroom', 'mushroom'],
-        imgUrl:
-          'https://cdn.jsdelivr.net/gh/ealush/emoji-picker-react@custom_emojis_assets/shroom.png',
-        id: 'shroom'
-      },]} />
+      <EmojiPicker
+        autoFocusSearch={false}
+        previewConfig={{ showPreview: false }}
+        width={300}
+        onEmojiClick={onEmojiClick}
+      />
     </div>
   );
 };
